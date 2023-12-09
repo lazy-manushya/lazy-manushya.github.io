68335b368ee4eae59e292fc5e07472e7cd1cb02d
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2023-12-09 19:07:21 +0530

    Add - Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>
+        Nothing to see here <u>stranger</u>, <br/>come back later but <u>soon</u>.
+      </h1>
+    </div>
+  );
 }
