04cbb6296e9a8c7da82b96fd1749f26720e22dd4
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2023-12-09 13:51:00 +0000

    Deploying to public from @ lazy-manushya/lazy-manushya.github.io@d799c6e0b672d89d97d040beab5964ca58ad4047 🚀

--- _next/static/J-hxHibjCoOCnCK26JAlc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9c0921fb8f46390d.css","static/chunks/pages/index-cccdbb2f3330bd6e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
