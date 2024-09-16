0f6c3c42e61b272ad0d6dda2664805a44bff6181
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2024-09-16 12:51:01 +0530

    Update site

---
+++ _next/static/K7-u_MFLBkGO1YLR2M0f1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7487ec503e10c0e8.css","static/chunks/pages/index-cb70da49ca4f8e86.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
